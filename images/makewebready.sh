cf8ddee06e747ba4464a3edfc68ee0cb328e857c
Steve <number5alive@gmail.com>
2021-01-13 21:48:17 +0000

    images: responsive pictures plus script to make them - need to convert others

---
+++ images/makewebready.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# Super lazy script
+# Strips EXIF and makes four new versions of an original file
+#
+# $ ./makewebready.sh originalfilename no .jpg
+#
+
+SRC_FILE="$1"
+echo $SRC_FILE
+
+exiftool -o $SRC_FILE-clean.jpg -all= $SRC_FILE.jpg
+convert $SRC_FILE-clean.jpg -quality 0 -resize 1024x $SRC_FILE-1024.jpg
+convert $SRC_FILE-clean.jpg -quality 0 -resize 1024x $SRC_FILE-1024.webp
+convert $SRC_FILE-clean.jpg -quality 0 -resize 640x $SRC_FILE-640.webp
+convert $SRC_FILE-clean.jpg -quality 0 -resize 320x $SRC_FILE-320.webp
+rm $SRC_FILE-clean.jpg
